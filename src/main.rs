abcb2aa3370eca21f495243486f39dfe48bb690d
Anoromi <and.zagorulko@gmail.com>
2025-01-03 11:49:24 +0000

    test 4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-// #[cfg(windows)]
-pub mod windows;
+use cfg_if::cfg_if;
+
+#[cfg(target_os = "windows")]
+pub mod winstuff;
 
 fn main() {
-    // if cfg!(windows) {
-        windows::hello_there().unwrap();
-    // }
+    let k = {
+        cfg_if!(
+            if #[cfg(target_os = "windows")] {
+                {
+                    use winstuff::hello_there;
+                    hello_there()
+                }
+            }
+            else {
+                let g = 3;
+                g.saturating_add(34);
+                3
+            }
+        )
+    };
 }
