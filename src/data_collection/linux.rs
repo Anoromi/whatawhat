f1aa6c51224f5079f65c8a727e84f1f88b8272f9
Anoromi <and.zagorulko@gmail.com>
2025-02-09 17:19:05 +0000

    test

--- src/data_collection/linux.rs
+++ src/data_collection/linux.rs
@@ -84,7 +84,7 @@ pub fn get_pid(conn: &Connection, window: Window) -> Result<()> {
         long_offset: 0,
         long_length: 1,
     }))?;
-    println!("{:?}", result.value::<u32>());
+    println!("pid {:?}", result.value::<u32>());
     Ok(())
 }
 
