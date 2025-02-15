1666661ad043ca36ced85b1b198637b93605ff12
Anoromi <and.zagorulko@gmail.com>
2025-02-15 17:26:20 +0000

    test

--- src/data_collection/linux.rs
+++ src/data_collection/linux.rs
@@ -1,10 +1,12 @@
 use std::ptr::null;
 
 use anyhow::Result;
+use sysinfo::Pid;
 use xcb::{
     screensaver::{QueryInfo, QueryInfoReply},
     x::{
-        self, Atom, GetInputFocus, GetProperty, GrabServer, InputFocus, InternAtom, InternAtomCookie, QueryTree, UngrabServer, Window, ATOM_ANY, ATOM_WINDOW
+        self, Atom, GetInputFocus, GetProperty, GrabServer, InputFocus, InternAtom,
+        InternAtomCookie, QueryTree, UngrabServer, Window, ATOM_ANY, ATOM_WINDOW,
     },
     Connection, Xid,
 };
@@ -74,17 +76,33 @@ pub fn get_pid_atom(conn: &Connection) -> Result<Atom> {
     Ok(reply.atom())
 }
 
-pub fn get_pid(conn: &Connection, window: Window) -> Result<()> {
+pub fn get_pid(conn: &Connection, window: Window) -> Result<Option<u32>> {
     let result = conn.wait_for_reply(conn.send_request(&GetProperty {
         delete: false,
         window,
         property: get_pid_atom(conn)?,
         r#type: ATOM_ANY,
         long_offset: 0,
-        long_length: 2,
+        long_length: 1,
     }))?;
-    println!("pid {:?}", result.value::<u32>());
-    Ok(())
+    let result_slice = result.value::<u32>();
+    println!("pid {:?}", result_slice);
+    if result_slice.len() == 0 {
+        return Ok(None);
+    }
+    Ok(Some(result_slice[0]))
+}
+
+pub fn get_process_name(id: u32) -> Result<Option<String>> {
+    let system = sysinfo::System::new_all();
+    let Some(process) = system.process(Pid::from_u32(id)) else {
+        return Ok(None);
+    };
+
+    Ok(process
+        .exe()
+        .and_then(|v| v.to_str())
+        .map(|v| v.to_string()))
 }
 
 pub fn get_active_window_atom(conn: &Connection) -> Result<Atom> {
@@ -107,7 +125,7 @@ fn get_active_window(conn: &Connection, window: Window) -> Result<Window> {
     Ok(result.value::<Window>()[0])
 }
 
-pub fn get_name(conn: &Connection, window: Window) -> Result<()> {
+pub fn get_name(conn: &Connection, window: Window) -> Result<String> {
     let wm_name = conn.wait_for_reply(conn.send_request(&x::GetProperty {
         delete: false,
         window,
@@ -118,8 +136,8 @@ pub fn get_name(conn: &Connection, window: Window) -> Result<()> {
     }))?;
     let title = String::from_utf8(wm_name.value().to_vec())
         .expect("The WM_NAME property is not valid UTF-8");
-    dbg!(title);
-    Ok(())
+    dbg!(&title);
+    Ok(title)
 }
 
 pub fn get_active_internal(conn: &Connection) -> Result<ActiveWindowData> {
@@ -136,7 +154,7 @@ pub fn get_active_internal(conn: &Connection) -> Result<ActiveWindowData> {
     let mut wnd = focus_reply.focus();
 
     get_name(conn, wnd)?;
-    get_pid(conn, wnd)?;
+    dbg!(get_pid(conn, wnd)?.map(get_process_name).transpose()?);
     loop {
         let tree = conn.wait_for_reply(conn.send_request(&QueryTree { window: wnd }))?;
 
@@ -146,7 +164,7 @@ pub fn get_active_internal(conn: &Connection) -> Result<ActiveWindowData> {
         } else {
             wnd = tree.parent();
             get_name(conn, wnd)?;
-            get_pid(conn, wnd)?;
+            dbg!(get_pid(conn, wnd)?.map(get_process_name).transpose()?);
         }
     }
 
@@ -154,7 +172,7 @@ pub fn get_active_internal(conn: &Connection) -> Result<ActiveWindowData> {
 
     // let wnd = get_active_window(conn, wnd)?;
 
-    get_pid(conn, wnd)?;
+    dbg!(get_pid(conn, wnd)?.map(get_process_name).transpose()?);
     let wm_name = conn.wait_for_reply(conn.send_request(&x::GetProperty {
         delete: false,
         window: wnd,
