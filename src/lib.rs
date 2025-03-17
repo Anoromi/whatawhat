b27f97fb8f8dc1833ee9dcda873e09373deff08f
Anoromi <and.zagorulko@gmail.com>
2025-03-17 17:42:14 +0000

    combine modules

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! Simple to use cli/daemon for analyzing your activity throughout the day.
+//! Unlike other solutions this doesn't require any runtimes, is quite lightweight, and can be
+//! easily used through a terminal.
+//!
+
+pub mod cli;
+pub mod daemon;
+pub mod fs;
+pub mod utils;
+pub mod window_api;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tracing::error;
+use whatawhat::cli::run_cli;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    run_cli().await.inspect_err(|e| {
+        error!("Error running cli {e:?}");
+    })?;
+    Ok(())
 }
+
